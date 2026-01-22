1d089960142adc54f54277949aad681c07bf67da
Binbasri-in <mohammed.binbasri@gmail.com>
2026-01-22 08:45:52 +0530

    lab1: added task file, wrote perfect makefile, added starting files

---
+++ include/student.h
@@ -0,0 +1,19 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#define MAX_STUDENTS 100
+#define SUBJECTS 5
+#define MAX_ID_LEN 20
+#define MAX_NAME_LEN 50
+
+typedef struct {
+    char id[MAX_ID_LEN];
+    char name[MAX_NAME_LEN];
+    int marks[SUBJECTS];
+    int total;
+    float percentage;
+    char grade[3];
+    float cgpa;
+} Student;
+
+#endif
